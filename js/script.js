379b20595c514d86701c73a63a078c3d510d2759
mkw98 <40563214+mkw98@users.noreply.github.com>
2018-09-18 21:22:21 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,13 +2,12 @@ function Phone(brand, price, color) {
 	this.brand = brand;
 	this.price = price;
 	this.color = color;
-}
+};
 
 Phone.prototype.printInfo = function() {
-		console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
-}
+	console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
+};
 
-var iPhone6S = new Phone("Apple", 2250, "silver");
-var Samsung Galaxy S6 = new Phone("Samsung", 900, "black");
+var SamsungGalaxyS6 = new Phone("Samsung", 900, "black");
 var iPhone6s = new Phone("Apple", 1500, "white");
-var OnePlus One = new Phone("OnePlus", 1300, "black");
+var OnePlusOne = new Phone("OnePlus", 1300", "black");
